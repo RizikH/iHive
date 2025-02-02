3358caa296cd07544e15baac482f84bf126668cc
Sam <sjbutttonow@uncg.edu>
2025-02-02 15:15:39 -0500

    Modified the Home Page

--- frontend/pages/investor.tsx
+++ frontend/pages/investor.tsx
@@ -30,7 +30,7 @@ const InvestorPage = () => {
 
     return (
         <>
-            <Head>
+            {/* <Head>
                 <meta charSet="UTF-8" />
                 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
                 <meta name="Author" content="Yixi Xie" />
@@ -41,7 +41,7 @@ const InvestorPage = () => {
                     href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css"
                 />
                 <link rel="icon" href="/Images/iHive.png" />
-            </Head>
+            </Head> */}
 
             <nav className={styles.navbar}>
                 <div className={styles.logo}>iHive - Investor</div>
