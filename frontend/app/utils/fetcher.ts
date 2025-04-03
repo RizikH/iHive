281ad4399a10f2fe3c15ad0d0676197c831bca61
Rizik Haddad <rihaddad2@uncg.edu>
2025-04-03 18:01:14 -0400

    fixed merge issues

--- frontend/app/utils/fetcher.ts
+++ frontend/app/utils/fetcher.ts
@@ -3,10 +3,6 @@ const API_URL =
     ? "https://ihive.onrender.com/api"
     : "http://localhost:5000/api";
 
-
-export const fetcher = async (path: string, options: RequestInit = {}) => {
-  console.log("Requesting:", `${API_URL}${path}`);
-
 export const fetcher = async (
   path: string,
   method: string = "GET",
@@ -15,7 +11,6 @@ export const fetcher = async (
   responseType: "json" | "blob" = "json"  // ✅ NEW PARAM
 ) => {
   const isFormData = body instanceof FormData;
-
 
   const res = await fetch(`${API_URL}${path}`, {
     method,
@@ -42,4 +37,3 @@ export const fetcher = async (
 
   return responseType === "blob" ? res.blob() : res.json();
 };
-}
