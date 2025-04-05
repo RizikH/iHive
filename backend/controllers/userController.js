9e849fd070c60a2098a497cd1aed639d9c7b551d
Rizik Haddad <rihaddad2@uncg.edu>
2025-04-05 16:05:46 -0400

    enabled secure cookies for production

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -64,7 +64,7 @@ const loginUser = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: true,
-      sameSite: "lax",
+      sameSite: "None",
       path: "/",
       maxAge: 24 * 60 * 60 * 1000
     });
